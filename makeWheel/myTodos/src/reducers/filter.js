ed78a0a6bb00a1a88ad4b1c961e200a44cfedadf
yahone <xz11111001001@hotmail.com>
2017-11-27 14:20:16 +0800

    delete useless

--- makeWheel/myTodos/src/reducers/filter.js
+++
@@ -1,8 +0,0 @@
-export default (state = 'show_all', action) => {
-    switch (action.filter) {
-        case 'FILTER_TODO':
-            return action.filter;
-        default:
-            return state;
-    }
-};
