189a7dbf2710afbb93efcc8b52330773a81f3775
yahone <xz11111001001@hotmail.com>
2018-05-17 16:09:04 +0800

    common update

--- makeWheel/vue-start/src/main.js
+++ makeWheel/vue-start/src/main.js
@@ -2,3 +2,17 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+let app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
