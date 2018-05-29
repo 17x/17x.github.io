3ffe1be4be250f4edb63b857b87f7bca00f53656
yahone <xz11111001001@hotmail.com>
2018-05-29 18:28:07 +0800

    update vue-start

--- makeWheel/vue-start/src/main.js
+++ makeWheel/vue-start/src/main.js
@@ -1,41 +1,1 @@
-let log = console.log;
-let warn = console.warn;
-
-import Vue from 'vue';
-
-let data = {
-    message: 'hello Vue.js !',
-    groceryList: [
-        {id: 0, text: 'Vegetables'},
-        {id: 1, text: 'Cheese'},
-        {id: 2, text: 'Whatever else humans are supposed to eat'}
-    ],
-    newMsg: ''
-};
-
-let vm = new Vue({
-    el: '#app',
-    data,
-    methods: {
-        outerEvent: function () {
-            log('outerEvent');
-        },
-        innerEvent: function () {
-            log('innerEvent');
-        }
-    },
-    watch: {
-        message: function (val) {
-            this.newMsg = 'new msg of ' + this.message;
-        }
-    },
-    computed: {
-        reversedMessage: function () {
-            return this.message.split('').reverse().join('');
-        }
-    }
-});
-
-setTimeout(() => {
-    vm.message = 'world !!';
-}, 2000);
+let log = console.log;let warn = console.warn;import Vue from 'vue';import axios from 'axios';import debounce from 'lodash/debounce';import capitalize from 'lodash/capitalize';new Vue({    el: '#app',    data: {        yourEnter: ''    },    methods: {        keyEvent: function (event) {            console.log(event.keyCode)        }    }});
